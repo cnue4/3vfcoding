2268707fb3086a875421dd163a2ca499ed26a447
cnue4 <49274771+cnue4@users.noreply.github.com>
2026-05-15 09:17:33 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -20,11 +20,11 @@ use think\facade\Env;
         // 服务器地址
         'hostname'          => env('DB_HOST', '127.0.0.1'),
         // 数据库名
-        'database'          => env('DB_NAME', 'codecrms_ygz_ink'),
+        'database'          => env('DB_NAME', '数据库用户名'),
         // 用户名
-        'username'          => env('DB_USER', 'codecrms_ygz_ink'),
+        'username'          => env('DB_USER', '数据库用户名'),
         // 密码
-        'password'          => env('DB_PASS', 'Ygz@qq.com123...'),
+        'password'          => env('DB_PASS', '数据库密码'),
         // 端口
         'hostport'          => env('DB_PORT', '3306'),
         // 数据库连接参数
